746a5b86fb845187f142aca972a6291181e7abab
Cohen Archbold <cgarchbold@gmail.com>
2021-10-13 14:36:33 -0400

    Some Aspects done

--- src/Gui.java
+++ src/Gui.java
@@ -1,24 +1,87 @@
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicArrowButton;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /*
 *  This class defines a gui for the polygon program.
 *
 * */
 
-public class Gui extends JFrame {
+public class Gui extends JFrame implements ActionListener {
+
+    JMenuBar menuBar;
+    JMenu File;
+    JButton reset, color;
+    JButton fill, play, stop ;
+    JLabel animation_sec;
+    Image i;
 
     public Gui(){
-        Image i = new Image();
+        i = new Image();
         add(i);
 
+        menuBar = new JMenuBar();
+        File = new JMenu("File");
+        reset = new JButton("Reset");
+        color = new JButton("Set Color");
+        fill = new JButton("Fill Poly");
+        animation_sec = new JLabel("Control:");
+        play = new JButton("Play");
+        stop = new JButton("Stop");
+
+        reset.addActionListener(this);
+        color.addActionListener(this);
+        fill.addActionListener(this);
+
+        menuBar.add(File);
+        menuBar.add(reset);
+        menuBar.add(color);
+        menuBar.add(fill);
+        menuBar.add(animation_sec);
+        menuBar.add(play);
+        menuBar.add(stop);
+        this.setJMenuBar(menuBar);
+
+        fill.setOpaque(true);
+        menuBar.setOpaque(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setSize(800,800);
         setVisible(true);
     }
 
+    public void reset(){
+        remove(i);
+        i = new Image();
+        add(i);
+        this.revalidate();
+    }
+
 
     public static void main(String[] args) {
         Gui g = new Gui();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == reset){
+            reset();
+        }
+        if(e.getSource() == color){
+            Color newColor = JColorChooser.showDialog(null, "Choose a color", Color.RED);
+            i.setColor(newColor);
+        }
+        if(e.getSource() == fill){
+            if(i.isPoly_fill()){
+                i.setPoly_fill(false);
+                fill.setBackground(null);
+            }
+            else{
+                i.setPoly_fill(true);
+                fill.setBackground(Color.LIGHT_GRAY);
+            }
+        }
     }
 }
--- src/Image.java
+++ src/Image.java
@@ -1,56 +1,104 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.util.ArrayList;
 
 
-public class Image extends JPanel implements MouseListener {
+public class Image extends JPanel implements MouseListener, MouseMotionListener, ActionListener {
     private Polygon poly;
+    private Color color;
+    private Timer timer;
 
-    //point array
+    //point array  DONT NEED
     ArrayList<Point> points;
 
     Point curr_clicked;
-    boolean mouse_pressed;
+    boolean mouse_pressed, poly_fill, animating;
+
+    private int rotation_degrees;
+
+
 
     public Image(){
         points = new ArrayList<Point>();
+        timer = new Timer(200, this); // every 200 ms
+        rotation_degrees = 0;
+
+        color = Color.RED;
+        poly_fill = false;
+        animating = false;
 
         addMouseListener(this);
+        addMouseMotionListener(this);
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+        repaint();
+    }
+    public void setPoly_fill(boolean fill){
+        this.poly_fill = fill;
+        repaint();
+    }
+    public void play(){
+        animating = true;
+        timer.start();
+    }
+    public void stop(){
+        animating =false;
+        timer.stop();
+    }
+
+    public boolean isPoly_fill() {return poly_fill;}
+
+    public Point centroid()  {
+        double centroidX = 0, centroidY = 0;
+
+        for(Point p : points) {
+            centroidX += p.getX();
+            centroidY += p.getY();
+        }
+        return new Point((int)(centroidX / points.size()), (int)(centroidY / points.size()));
     }
 
     public void paint(Graphics g){
         super.paint(g);
 
-        //draw points in array
-        for(Point p: points)
-            g.fillRect(p.x-2, p.y-2, 4, 4);
+        if(animating){
 
-        //draw lines connecting them
-        g.setColor(Color.BLUE);
-        for(int i=0; i< points.size()-1;i++){
-            Point s = points.get(i);
-            Point e = points.get(i+1);
-            g.drawLine(s.x,s.y,e.x,e.y);
-            //System.out.println("line drawn ("+s.x+", "+s.y+"):("+e.x+", "+e.y+")");
         }
-        //if points size is greater than 2, connect the ends
-        if(points.size() > 2) {
-            Point s = points.get(points.size() - 1);
-            Point e = points.get(0);
-            g.drawLine(s.x,s.y,e.x,e.y);
-            //System.out.println("line drawn ("+s.x+", "+s.y+"):("+e.x+", "+e.y+")");
-        }
+        else {
+            //draw lines connecting them
+            g.setColor(color);
 
+            make_polygon();
+            if (poly_fill)
+                g.fillPolygon(poly);
+            else
+                g.drawPolygon(poly);
+
+            g.setColor(Color.BLACK);
+            //draw points in array
+            //if we arent animating
+            for (Point p : points)
+                g.fillRect(p.x - 2, p.y - 2, 4, 4);
+        }
+    }
+
+    public void make_polygon(){
+        poly = new Polygon();
+        for(Point p: points){
+            poly.addPoint(p.x, p.y);
+        }
     }
 
     public Point check_point(Point q){
         Point nearest = q;
 
         for(Point p: points){
-            if(q.distance(p)<=2)
+            //System.out.println(q.distance(p));
+            if(q.distance(p)<=5)
                 nearest = p;
         }
         return nearest;
@@ -75,7 +123,8 @@ public class Image extends JPanel implements MouseListener {
         if(curr_clicked == null) {
             Point clik = new Point(x, y);
             curr_clicked = check_point(clik);
-            points.add(curr_clicked);
+            if(!points.contains(curr_clicked))
+                points.add(curr_clicked);
         }
         curr_clicked.setLocation(x, y);
         repaint();
@@ -95,5 +144,28 @@ public class Image extends JPanel implements MouseListener {
     @Override
     public void mouseExited(MouseEvent e) {
 
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        if(mouse_pressed){
+            int x = e.getX();
+            int y = e.getY();
+            curr_clicked.setLocation(x,y);
+            repaint();
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == timer){
+            rotation_degrees +=1;
+            repaint();
+        }
     }
 }
